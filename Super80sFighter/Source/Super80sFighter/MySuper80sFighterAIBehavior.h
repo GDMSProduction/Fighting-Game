016796b9e908b577cd5c7b95766fdacf8dc040c7
Shawn Kyler <SPKyler@fullsail.edu>
2018-01-25 17:45:43 -0500

    Added test functionality to AI and AI controls.

--- Super80sFighter/Source/Super80sFighter/MySuper80sFighterAIBehavior.h
+++ Super80sFighter/Source/Super80sFighter/MySuper80sFighterAIBehavior.h
@@ -7,5 +7,15 @@
 UCLASS()
 class SUPER80SFIGHTER_API AMySuper80sFighterAIBehavior : public ASuper80sFighterCharacter
 {	
-	GENERATED_BODY();
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Do")
+
+	void Do();
+	UFUNCTION(BlueprintCallable, Category = "Do")
+	void Dont(int blarb);
+
+	//Increment the timer, called every frame.
+	virtual void Tick(float DeltaTime) override;
 };
