232e972516aae301ef8578d8daedfabd902b5ab8
Shawn Kyler <SPKyler@fullsail.edu>
2018-01-30 19:29:37 -0500

    Added fixed functionality to the random component of the Thug, as well as changes to the blueprint moves for the Thug.

--- Super80sFighter/Source/Super80sFighter/ThugClass.cpp
+++ Super80sFighter/Source/Super80sFighter/ThugClass.cpp
@@ -14,49 +14,46 @@ void AThugClass::SetUpThug()
 	inputsToUse.Add(KICK);
 
 	//Determine the moves the thug will have available to them.
-	DetermineMoveSet(inputsToUse, stageNumber);
+	DetermineMoveSet(inputsToUse, difficultyLevel);
 }
 
 //Determine the moves the thug will have available to them.
-void AThugClass::DetermineMoveSet(TArray<short>& _inputsToUse, short _stage)
+void AThugClass::DetermineMoveSet(TArray<short>& _inputsToUse, short _difficultyLevel)
 {
 	//Determine the stage the player is on.
-	switch (_stage)
+	switch (_difficultyLevel)
 	{
 	case 0:
+		_inputsToUse.Add(SPECIAL1);
 		break;
 
 	case 1:
-		break;
-
-	case 2:
-		_inputsToUse.Add(SPECIAL1);
-		break;
-
-	case 3:
-		break;
-
-	case 4:
-		_inputsToUse.Add(SPECIAL1);
-		_inputsToUse.Add(SPECIAL2);
-		break;
-
-	case 5:
-		break;
-
-	case 6:
 		_inputsToUse.Add(SPECIAL1);
 		_inputsToUse.Add(SPECIAL2);
 		_inputsToUse.Add(SPECIAL3);
 		break;
 
-	case 7:
+	case 2:
 		_inputsToUse.Add(SPECIAL1);
 		_inputsToUse.Add(SPECIAL2);
 		_inputsToUse.Add(SPECIAL3);
 		_inputsToUse.Add(SPECIAL4);
 		break;
+
+	default:
+			break;
 	}
+}
+
+//Choose the next move for the thug to perform.
+void AThugClass::ChooseNextMove()
+{
+	Crouch();
+	PressPunch();
+	PressKick();
+	PressSpecial();
+	PressShortHop();
+	PressHighJump();
 }
 
 //Increment the timer, called every frame.
@@ -65,8 +62,7 @@ void AThugClass::Tick(float _deltaTime)
 	//Use the parent's timer to perform the tick.
 	Super::Tick(_deltaTime);
 
-	spawnHitbox(EHITBOX_TYPE::VE_HITBOX_GET_PAINBOX, FVector(0, 0, -80), FVector(.5f, .5f, 1.5f), 0);
-	spawnHitbox(EHITBOX_TYPE::VE_HITBOX_GET_THROWBOX, FVector(0, 0, -60), FVector(.35f, .35f, 1.25f), 0);
+	ChooseNextMove();
 }
 
 
