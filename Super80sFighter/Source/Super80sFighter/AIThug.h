42ddab3614bcf3a30e278e63f29e48914a075330
Matthew Moody <kratoes123@live.com>
2018-02-20 18:51:25 -0500

    Added the bare file for the first AI thug Fixed camera position for Thursday 2/22/2018

---
+++ Super80sFighter/Source/Super80sFighter/AIThug.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIBase.h"
+
+/**
+ * 
+ */
+class SUPER80SFIGHTER_API AIThug : public AIBase
+{
+public:
+	AIThug();
+	~AIThug();
+};
